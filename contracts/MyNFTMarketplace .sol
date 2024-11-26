3975f8417e8cfb4733e84210055a4aca8211191d
techminer <tech.miner95@gmail.com>
2024-11-26 15:55:11 +0900

    smartcontract

--- contracts/MyNFTMarketplace .sol
+++ contracts/MyNFTMarketplace .sol
@@ -3,20 +3,20 @@ pragma solidity ^0.8.27;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyNFTMarketPlace is ERC721, Ownable {
-    using Counters for Counters.Counter;
-
-    Counters.Counter private _tokenIdCounter;
+abstract contract MyNFTMarketplace is ERC721, Ownable {
+    constructor() ERC721("PetNFT", "PNFT") {}
+    uint256 private _tokenIdCounter;
 
     struct NFT {
         string name;
-        uint8 price;
+        uint256 price;
         address owner;
     }
+
     mapping(uint256 => NFT) public nfts;
-     mapping(address => uint256[]) private _ownedTokens;
+    mapping(address => uint256[]) private _ownedTokens;
+
     event NFTCreated(
         uint256 tokenId,
         string name,
@@ -24,58 +24,65 @@ contract MyNFTMarketPlace is ERC721, Ownable {
         address owner
     );
     event NFTTransferred(uint256 tokenId, address from, address to);
-    event NFTPriceUpdated(uint256 tokenId, uint256 oldPrice, uint newPrice);
-
-    constructor() ERC721("PetNFT", "PNFT");
+    event NFTPriceUpdated(uint256 tokenId, uint256 oldPrice, uint256 newPrice);
 
     function mintNFT(string memory name, uint256 price) public onlyOwner {
-        _tokenIdCounter.increment();
-        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter++;
+        uint256 tokenId = _tokenIdCounter;
 
         _mint(msg.sender, tokenId);
 
         nfts[tokenId] = NFT(name, price, msg.sender);
+        _ownedTokens[msg.sender].push(tokenId);
 
-         _ownedTokens[msg.sender].push(tokenId);
-
-        emit NFTCreated(tokenId, name, price, nsg.sender);
+        emit NFTCreated(tokenId, name, price, msg.sender);
     }
 
-    function transsferNFT(uint256 tokenId, address to) public {
-        require(
-            owerOf(tokenId) == msg.sender,
-            "You are not the owner of this NFT"
-        );
+    function transferNFT(uint256 tokenId, address to) public {
+        address from = ownerOf(tokenId);
 
-        _transfer(msg.sender, to, tokenId);
+        require(from == msg.sender, "You are not the owner of this NFT");
+
+        _transfer(from, to, tokenId);
 
         nfts[tokenId].owner = to;
 
-         _removeTokenFromOwnerEnumeration(from, tokenId);
+        _removeTokenFromOwnerEnumeration(from, tokenId);
         _ownedTokens[to].push(tokenId);
 
-        emit NFTTransferred(tokenId, msg.sender, to);
+        emit NFTTransferred(tokenId, from, to);
     }
+
     function updatePrice(uint256 tokenId, uint256 newPrice) public {
-        require(ownerOf(tokenId) == msg.sender, "You are not the owner of this NFT");
-        uint8 oldPrice = nfts[toekndId].price;
+        require(
+            ownerOf(tokenId) == msg.sender,
+            "You are not the owner of this NFT"
+        );
+
+        uint256 oldPrice = nfts[tokenId].price;
         nfts[tokenId].price = newPrice;
+
         emit NFTPriceUpdated(tokenId, oldPrice, newPrice);
     }
-    function getNFT(uint256 tokenId) public view returns (string memory, uint256, address){
-        require(_exists(tokenId), "NFT does not exist");
-        NFT memory nft = nfts(tokenId);
+
+    function getNFT(
+        uint256 tokenId
+    ) public view returns (string memory, uint256, address) {
+        NFT memory nft = nfts[tokenId];
         return (nft.name, nft.price, nft.owner);
     }
-    function getNFTsByOwner(address owner) 
-        public 
-        view 
-        returns (uint256[] memory tokenIds) 
-    {
+
+    function getNFTsByOwner(
+        address owner
+    ) public view returns (uint256[] memory tokenIds) {
         return _ownedTokens[owner];
     }
+
     // Internal function to remove a token from the owner's list
-    function _removeTokenFromOwnerEnumeration(address from, uint256 tokenId) private {
+    function _removeTokenFromOwnerEnumeration(
+        address from,
+        uint256 tokenId
+    ) private {
         uint256[] storage ownedTokens = _ownedTokens[from];
         for (uint256 i = 0; i < ownedTokens.length; i++) {
             if (ownedTokens[i] == tokenId) {
